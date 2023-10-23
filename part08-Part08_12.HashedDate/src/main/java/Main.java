b4e7aa3cbd4a0e0f0d9998191b8acfce60e3e496
agaolejniq <a.olejnikk@gmail.com>
2023-10-23 21:33:13 +0200

    'Similarity of objects' hash value

--- part08-Part08_12.HashedDate/src/main/java/Main.java
+++ part08-Part08_12.HashedDate/src/main/java/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         // make test programs here
+        SimpleDate d = new SimpleDate(1, 2, 2000);
+        SimpleDate d2 = new SimpleDate(2, 2, 1990);
+        
+        System.out.println(d.hashCode());
+        System.out.println(d2.hashCode());
+        
 
     }
 }
