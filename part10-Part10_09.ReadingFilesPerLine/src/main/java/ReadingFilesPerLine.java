95dad95e3e52dde25b2908e747e5aafe6d88f0f2
agaolejniq <a.olejnikk@gmail.com>
2024-02-24 12:12:11 +0100

    streams, comparable interface

--- part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -10,8 +10,16 @@ public class ReadingFilesPerLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test the method here
-
     }
 
+    public static List<String> read(String file) {
+        List<String> lines = new ArrayList<>();
+
+        try {
+            Files.lines(Paths.get(file)).forEach(line -> lines.add(line));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lines;
+    }
 }
--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -10,8 +10,24 @@ public class BooksFromFile {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test your method here
+        System.out.println(readBooks("books.txt"));
 
     }
 
+    public static List<Book> readBooks(String file) {
+        List<Book> books = new ArrayList<>();
+        
+        try {
+            Files.lines(Paths.get(file))
+                    .map(row -> row.split(","))
+                    .map(row_elements -> new Book(row_elements[0], Integer.valueOf(row_elements[1]),
+                    Integer.valueOf(row_elements[2]), row_elements[3]))
+                    .forEach(book -> books.add(book));
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return books;
+    }
+    
 }
